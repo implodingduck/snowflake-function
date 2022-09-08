7c3e862a4887920632452909de774a1421da6711
implodingduck <zsek320@gmail.com>
2022-09-08 11:20:39 -0500

    adding a function

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,3 +49,291 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
   tags     = local.tags
 }
+
+
+resource "azurerm_virtual_network" "default" {
+  name                = "vnet-${local.func_name}-${local.loc_for_naming}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.9.0.0/24"]
+
+  tags = local.tags
+}
+
+resource "azurerm_subnet" "pe" {
+  name                  = "snet-privateendpoints-${local.loc_for_naming}"
+  resource_group_name   = azurerm_virtual_network.default.resource_group_name
+  virtual_network_name  = azurerm_virtual_network.default.name
+  address_prefixes      = ["10.9.0.0/26"]
+
+  enforce_private_link_endpoint_network_policies = true
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "pe" {
+  subnet_id                 = azurerm_subnet.pe.id
+  network_security_group_id = data.azurerm_network_security_group.basic.id
+}
+
+resource "azurerm_subnet" "functions" {
+  name                  = "snet-functions-${local.loc_for_naming}"
+  resource_group_name   = azurerm_virtual_network.default.resource_group_name
+  virtual_network_name  = azurerm_virtual_network.default.name
+  address_prefixes      = ["10.9.0.64/26"]
+  service_endpoints = [
+    "Microsoft.Web",
+    "Microsoft.Storage"
+  ]
+  delegation {
+    name = "serverfarm-delegation"
+    service_delegation {
+      name = "Microsoft.Web/serverFarms"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/action",
+      ]
+    }
+  }
+ 
+}
+
+resource "azurerm_subnet_network_security_group_association" "functions" {
+  subnet_id                 = azurerm_subnet.functions.id
+  network_security_group_id = data.azurerm_network_security_group.basic.id
+}
+
+resource "azurerm_private_dns_zone" "blob" {
+  name                      = "privatelink.blob.core.windows.net"
+  resource_group_name       = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "blob" {
+  name                  = "blob"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.blob.name
+  virtual_network_id    = azurerm_virtual_network.default.id
+}
+
+resource "azurerm_private_dns_zone" "functions" {
+  name                      = "privatelink.azurewebsites.net"
+  resource_group_name       = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_endpoint" "pe" {
+  name                = "pe-sa${local.func_name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.pe.id
+
+  private_service_connection {
+    name                           = "pe-connection-sa${local.func_name}"
+    private_connection_resource_id = azurerm_storage_account.sa.id
+    is_manual_connection           = false
+    subresource_names              = ["blob"]
+  }
+  private_dns_zone_group {
+    name                 = azurerm_private_dns_zone.blob.name
+    private_dns_zone_ids = [azurerm_private_dns_zone.blob.id]
+  }
+}
+
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "sa${local.func_name}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "function"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
+
+
+resource "azurerm_storage_container" "hosts" {
+  name                  = "azure-webjobs-hosts"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "secrets" {
+  name                  = "azure-webjobs-secrets"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
+
+resource "azurerm_user_assigned_identity" "uai" {
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  name = "uai-${local.func_name}"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                       = "kv-${local.func_name}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+  
+}
+
+resource "azurerm_key_vault_access_policy" "sp" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
+  
+  key_permissions = [
+    "Create",
+    "Get",
+    "Purge",
+    "Recover",
+    "Delete"
+  ]
+
+  secret_permissions = [
+    "Set",
+    "Purge",
+    "Get",
+    "List",
+    "Delete"
+  ]
+
+  certificate_permissions = [
+    "Purge"
+  ]
+
+  storage_permissions = [
+    "Purge"
+  ]
+  
+}
+
+
+resource "azurerm_key_vault_access_policy" "uai" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = azurerm_user_assigned_identity.uai.principal_id
+  
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+    "List"
+  ]
+  
+}
+
+resource "azurerm_key_vault_secret" "saconnstr" {
+  depends_on = [
+    azurerm_key_vault_access_policy.sp
+  ]
+  name         = "saconnstr"
+  value        = azurerm_storage_account.sa.primary_connection_string
+  key_vault_id = azurerm_key_vault.kv.id
+  tags         = {}
+}
+
+
+resource "azurerm_role_assignment" "uai" {
+  scope                = azurerm_storage_account.sa.id
+  role_definition_name = "Storage Blob Data Owner"
+  principal_id         = azurerm_user_assigned_identity.uai.principal_id
+}
+
+resource "azurerm_role_assignment" "system" {
+  scope                = azurerm_storage_account.sa.id
+  role_definition_name = "Storage Blob Data Owner"
+  principal_id         = azurerm_linux_function_app.func.identity.0.principal_id  
+}
+
+
+resource "azurerm_service_plan" "asp" {
+  name                = "asp-${local.func_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "EP1"
+}
+
+resource "azurerm_linux_function_app" "func" {
+  depends_on = [
+    azurerm_role_assignment.uai,
+    azurerm_storage_container.hosts,
+    azurerm_storage_container.secrets,
+    azurerm_key_vault_access_policy.uai
+  ]
+  name                = local.func_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  key_vault_reference_identity_id = azurerm_user_assigned_identity.uai.id
+  #storage_account_name            = azurerm_storage_account.sa.name
+  storage_key_vault_secret_id     = azurerm_key_vault_secret.saconnstr.id
+  #storage_uses_managed_identity   = true
+  service_plan_id                 = azurerm_service_plan.asp.id
+  virtual_network_subnet_id              = azurerm_subnet.functions.id
+
+  site_config {
+    application_insights_key = azurerm_application_insights.app.instrumentation_key
+    application_stack {
+      python_version = "3.8"
+    }
+    
+  }
+  identity {
+    type         = "SystemAssigned, UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.uai.id]
+  }
+  app_settings = {
+    "SCM_DO_BUILD_DURING_DEPLOYMENT"           = "1"
+    "BUILD_FLAGS"                              = "UseExpressBuild"
+    "ENABLE_ORYX_BUILD"                        = "true"
+    "XDG_CACHE_HOME"                           = "/tmp/.cache"
+    "FUNC_TYPE"                                = "USELOCAL"
+  }
+
+}
+
+resource "local_file" "localsettings" {
+    content     = <<-EOT
+{
+  "IsEncrypted": false,
+  "Values": {
+    "FUNCTIONS_WORKER_RUNTIME": "python",
+    "AzureWebJobsStorage": ""
+  }
+}
+EOT
+    filename = "../func/local.settings.json"
+}
+
+
+resource "null_resource" "publish_func" {
+  depends_on = [
+    azurerm_linux_function_app.func,
+    local_file.localsettings
+  ]
+  triggers = {
+    index = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    working_dir = "../func"
+    command     = "timeout 10m func azure functionapp publish ${azurerm_linux_function_app.func.name} --build remote"
+    
+  }
+}
+
+resource "azurerm_application_insights" "app" {
+  name                = "${local.func_name}-insights"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  application_type    = "other"
+  workspace_id        = data.azurerm_log_analytics_workspace.default.id
+}
