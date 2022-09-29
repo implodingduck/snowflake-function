35a8ed806f141de6047d1e4c952d89859368761d
implodingduck <zsek320@gmail.com>
2022-09-29 11:22:40 -0500

    adding more handling

--- func/TimerTrigger/__init__.py
+++ func/TimerTrigger/__init__.py
@@ -4,34 +4,38 @@ import os
 import socket
 import snowflake.connector
 import azure.functions as func
-
+import time
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
+    utc_timestamp_start = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
-    logging.info('Python timer trigger function start at %s', utc_timestamp)
-    conn = snowflake.connector.connect(
-        user=os.environ.get('SF_USER'),
-        password=os.environ.get('SF_PASS'),
-        account=os.environ.get('SF_ACCOUNT'),
-        insecure_mode = True,
-    )
+    logging.info('Python timer trigger function start at %s', utc_timestamp_start)
 
-    conn.cursor().execute("USE DATABASE SNOWFLAKE_SAMPLE_DATA")
-    cur = conn.cursor()
     try:
-        cur.execute("SHOW TABLES")
-        for r in cur:
-            logging.info(f"${r}")
-    except Exception as e:
-        logging.error(f"Something went wrong...\n${e}")
-    finally:
-        cur.close()
+        conn = snowflake.connector.connect(
+            user=os.environ.get('SF_USER'),
+            password=os.environ.get('SF_PASS'),
+            account=os.environ.get('SF_ACCOUNT'),
+            insecure_mode = True,
+        )
 
-    utc_timestamp = datetime.datetime.utcnow().replace(
+        conn.cursor().execute("USE DATABASE SNOWFLAKE_SAMPLE_DATA")
+        cur = conn.cursor()
+        try:
+            cur.execute("SHOW TABLES")
+            for r in cur:
+                logging.info(f"${r}")
+        except Exception as e:
+            logging.error(f"Something went wrong...\n${e}")
+        finally:
+            cur.close()
+    except Exception as e:
+        logging.error(f"Snowflake error: ${e}")
+
+    utc_timestamp_end = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
-    logging.info('Python timer trigger function done %s', utc_timestamp)
+    logging.info('Python timer trigger function done %s', utc_timestamp_end)
     
