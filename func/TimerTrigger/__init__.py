32fa4c03684e50bb5002ddb85dd9ac2bee770900
implodingduck <zsek320@gmail.com>
2022-10-10 11:58:07 -0500

    updating query

--- func/TimerTrigger/__init__.py
+++ func/TimerTrigger/__init__.py
@@ -43,7 +43,7 @@ class MySnowFlake():
         self.execute_query("SHOW TABLES")
         self.execute_query("use warehouse myxsmallwarehouse")
         self.execute_query("use schema snowflake_sample_data.tpch_sf1")
-        results = self.execute_query("SELECT l_returnflag, l_linestatus, sum(l_quantity) as sum_qty, sum(l_extendedprice) as sum_base_price, sum(l_extendedprice * (1-l_discount)) as sum_disc_price, sum(l_extendedprice * (1-l_discount) * (1+l_tax)) as sum_charge, avg(l_quantity) as avg_qty, avg(l_extendedprice) as avg_price, avg(l_discount) as avg_disc, count(*) as count_order FROM lineitem WHERE _shipdate <= dateadd(day, -90, to_date('1998-12-01')) GROUP BY l_returnflag, l_linestatus ORDER BY l_returnflag, l_linestatus")
+        results = self.execute_query("SELECT l_returnflag, l_linestatus, sum(l_quantity) as sum_qty, sum(l_extendedprice) as sum_base_price, sum(l_extendedprice * (1-l_discount)) as sum_disc_price, sum(l_extendedprice * (1-l_discount) * (1+l_tax)) as sum_charge, avg(l_quantity) as avg_qty, avg(l_extendedprice) as avg_price, avg(l_discount) as avg_disc, count(*) as count_order FROM lineitem WHERE l_shipdate <= dateadd(day, -90, to_date('1998-12-01')) GROUP BY l_returnflag, l_linestatus ORDER BY l_returnflag, l_linestatus")
         for r in results:
                 logging.info(f"{r}")
 
