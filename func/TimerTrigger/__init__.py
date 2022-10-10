e6815fd1d8e1a96a12c69d58db6a9ef84dfd79a4
implodingduck <zsek320@gmail.com>
2022-10-10 11:01:55 -0500

    slightly more complex query

--- func/TimerTrigger/__init__.py
+++ func/TimerTrigger/__init__.py
@@ -14,6 +14,43 @@ for logger_name in ['snowflake.connector']:
     ch.setFormatter(logging.Formatter('%(asctime)s - %(threadName)s %(filename)s:%(lineno)d - %(funcName)s() - %(levelname)s - %(message)s'))
     logger.addHandler(ch)
 
+class MySnowFlake():
+    def __init__(self, account=f"{os.environ.get('SF_ACCOUNT')}.privatelink", user=os.environ.get('SF_USER'), password=os.environ.get('SF_PASS')):
+        logging.info("MySnowFlake init...")
+        self.conn = snowflake.connector.connect(
+            user=user,
+            password=password,
+            account=account,
+        )
+        self.cursor = self.con.cursor()
+
+    def close_con(self):
+        logger.info("MySnowFlake Close Con...")
+        self.cursor.close()
+        self.con.close()
+
+    def execute_query(self, query):
+        try:
+            logger.info(f"MySnowflake Execute Query: {query}")
+            self.cursor.execute(query)
+            results = self.cursor.fetchall()
+        except Exception as e:
+            logger.info(f"MySnowflake Execute Query Error: {e}")
+        else:
+            return results
+
+    def do_stuff(self) -> None:
+        self.execute_query("SHOW TABLES")
+        self.execute_query("use warehouse myxsmallwarehouse")
+        self.execute_query("use schema snowflake_sample_data.tpch_sf1")
+        results = self.execute_query("SELECT l_returnflag, l_linestatus, sum(l_quantity) as sum_qty, sum(l_extendedprice) as sum_base_price, sum(l_extendedprice * (1-l_discount)) as sum_disc_price, sum(l_extendedprice * (1-l_discount) * (1+l_tax)) as sum_charge, avg(l_quantity) as avg_qty, avg(l_extendedprice) as avg_price, avg(l_discount) as avg_disc, count(*) as count_order FROM lineitem WHERE _shipdate <= dateadd(day, -90, to_date('1998-12-01')) GROUP BY l_returnflag, l_linestatus ORDER BY l_returnflag, l_linestatus")
+        for r in results:
+                logging.info(f"{r}")
+
+
+
+
+
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp_start = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
@@ -23,26 +60,10 @@ def main(mytimer: func.TimerRequest) -> None:
     logging.info('Python timer trigger function start at %s', utc_timestamp_start)
 
     try:
-        conn = snowflake.connector.connect(
-            user=os.environ.get('SF_USER'),
-            password=os.environ.get('SF_PASS'),
-            account=f"{os.environ.get('SF_ACCOUNT')}.privatelink",
-
-            #insecure_mode = True,
-        )
-
-        conn.cursor().execute("USE DATABASE SNOWFLAKE_SAMPLE_DATA")
-        cur = conn.cursor()
-        try:
-            cur.execute("SHOW TABLES")
-            for r in cur:
-                logging.info(f"{r}")
-        except Exception as e:
-            logging.error(f"Something went wrong...\n{e}")
-        # finally:
-        #     cur.close()
+        msf = MySnowFlake()
+        msf.do_stuff()
     except Exception as e:
-        logging.error(f"Snowflake error: {e}")
+        logging.error(f"Function error: {e}")
 
     utc_timestamp_end = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
